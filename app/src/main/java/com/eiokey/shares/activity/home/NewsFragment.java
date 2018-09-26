12588bf224b3ed18ab9e76fe9dc4ee53c4062bc3
JetQiao <qiao522600806@gmail.com>
2018-09-26 11:54:43 +0800

    第一次提交

---
+++ app/src/main/java/com/eiokey/shares/activity/home/NewsFragment.java
@@ -0,0 +1,20 @@
+package com.eiokey.shares.activity.home;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eiokey.shares.R;
+
+public class NewsFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View newsLayout = inflater.inflate(R.layout.news_layout, container, false);
+        return newsLayout;
+    }
+
+}
